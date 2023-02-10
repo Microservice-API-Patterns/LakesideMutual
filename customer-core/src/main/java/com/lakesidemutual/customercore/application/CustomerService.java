aaebc590832c9ffc064fa3a22eae20db17ab31d9
Mirko Stocker <mirko@stocker.email>
2023-02-10 20:43:22 +0100

    replace pagination implementation with a more efficient one

--- customer-core/src/main/java/com/lakesidemutual/customercore/application/CustomerService.java
+++ customer-core/src/main/java/com/lakesidemutual/customercore/application/CustomerService.java
@@ -1,31 +1,31 @@
 package com.lakesidemutual.customercore.application;
 
+import com.lakesidemutual.customercore.domain.customer.*;
+import com.lakesidemutual.customercore.infrastructure.CustomerRepository;
+import org.hibernate.jpa.QueryHints;
+import org.microserviceapipatterns.domaindrivendesign.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.microserviceapipatterns.domaindrivendesign.ApplicationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Component;
-
-import com.lakesidemutual.customercore.domain.customer.Address;
-import com.lakesidemutual.customercore.domain.customer.CustomerAggregateRoot;
-import com.lakesidemutual.customercore.domain.customer.CustomerFactory;
-import com.lakesidemutual.customercore.domain.customer.CustomerId;
-import com.lakesidemutual.customercore.domain.customer.CustomerProfileEntity;
-import com.lakesidemutual.customercore.infrastructure.CustomerRepository;
 
 /**
  * The CustomerService class is an application service that is
  * responsible for creating, updating and retrieving customer entities.
- * */
+ */
 @Component
 public class CustomerService implements ApplicationService {
 	@Autowired
 	private CustomerRepository customerRepository;
+
+	@Autowired
+	private EntityManager entityManager;
 
 	@Autowired
 	private CustomerFactory customerFactory;
@@ -61,37 +61,50 @@ public class CustomerService implements ApplicationService {
 	}
 
 	public List<CustomerAggregateRoot> getCustomers(String ids) {
-		List<CustomerId> customerIds = Arrays.asList(ids.split(",")).stream().map(id -> new CustomerId(id.trim())).collect(Collectors.toList());
+		List<CustomerId> customerIds = Arrays.stream(ids.split(",")).map(id -> new CustomerId(id.trim())).collect(Collectors.toList());
 
 		List<CustomerAggregateRoot> customers = new ArrayList<>();
 		for (CustomerId customerId : customerIds) {
 			Optional<CustomerAggregateRoot> customer = customerRepository.findById(customerId);
-			if (customer.isPresent()) {
-				customers.add(customer.get());
-			}
+			customer.ifPresent(customers::add);
 		}
 		return customers;
 	}
 
 	public Page<CustomerAggregateRoot> getCustomers(String filter, int limit, int offset) {
-		final List<CustomerAggregateRoot> filteredCustomers = customerRepository
-				.findAll(Sort.by(Sort.Direction.ASC, "customerProfile.firstname", "customerProfile.lastname")).stream()
-				.filter(customer -> matchesFilter(customer, filter))
-				.collect(Collectors.toList());
-		final int size = filteredCustomers.size();
-		final List<CustomerAggregateRoot> customers = filteredCustomers.stream().skip(offset).limit(limit).collect(Collectors.toList());
-		return new Page<>(customers, offset, limit, size);
-	}
 
-	private boolean matchesFilter(CustomerAggregateRoot customer, String filter) {
-		final String[] searchTerms = filter.toLowerCase().split(" ");
-		final String firstname = customer.getCustomerProfile().getFirstname().toLowerCase();
-		final String lastname = customer.getCustomerProfile().getLastname().toLowerCase();
-		for (String searchTerm : searchTerms) {
-			if (!searchTerm.isEmpty() && !firstname.contains(searchTerm) && !lastname.contains(searchTerm)) {
-				return false;
-			}
-		}
-		return true;
+		// See https://vladmihalcea.com/fix-hibernate-hhh000104-entity-fetch-pagination-warning-message/
+		// for details on the following implementation:
+
+		String filterParameter = "%" + filter + "%";
+
+		long totalSize = entityManager.createQuery(
+						"select count(1) from CustomerAggregateRoot c " +
+								"left join fetch c.customerProfile " +
+								"where c.customerProfile.firstname like :filter or c.customerProfile.lastname like :filter", Long.class)
+				.setParameter("filter", filterParameter)
+				.getSingleResult();
+
+		List<CustomerId> customerIds = entityManager.createQuery(
+						"select c.id from CustomerAggregateRoot c " +
+								"left join fetch c.customerProfile " +
+								"where c.customerProfile.firstname like :filter or c.customerProfile.lastname like :filter " +
+								"order by c.customerProfile.firstname, c.customerProfile.lastname", CustomerId.class)
+				.setParameter("filter", filterParameter)
+				.setFirstResult(offset)
+				.setMaxResults(limit)
+				.getResultList();
+
+		List<CustomerAggregateRoot> customerAggregateRoots = entityManager.createQuery(
+						"select c from CustomerAggregateRoot c " +
+								"left join fetch c.customerProfile " +
+								"left join fetch c.customerProfile.moveHistory " +
+								"where c.id in (:customerIds) " +
+								"order by c.customerProfile.firstname, c.customerProfile.lastname", CustomerAggregateRoot.class)
+				.setParameter("customerIds", customerIds)
+				.setHint(QueryHints.HINT_PASS_DISTINCT_THROUGH, false)
+				.getResultList();
+
+		return new Page<>(customerAggregateRoots, offset, limit, (int) totalSize);
 	}
 }
