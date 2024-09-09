98a4e7166e950f76ca5833e64a30695d5a3e52a1
Lukas Messmer <lukas.messmer@ost.ch>
2024-09-09 16:21:38 +0200

    Update App.js to work with refactored components

--- customer-management-frontend/src/App.js
+++ customer-management-frontend/src/App.js
@@ -10,52 +10,41 @@ import {Provider} from "react-redux"
 import {store} from "./store/store"
 import {customerManagementBackend, customerSelfServiceBackend, policyManagementFrontend,} from "./config"
 import Home from "./pages/Home";
+import NewCustomer from "./pages/NewCustomer";
+import CustomerDetail from "./pages/CustomerDetail";
+import EditCustomer from "./pages/EditCustomer";
 
-export default class App extends React.Component<{}> {
-    render() {
-        return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Container text>
-                        <SiteHeader/>
-
-                        <MenuBar/>
-
-            <Routes>
-              <Route exact path="/" render={(props) => <Home {...props} />} />
-              <Route
-                exact
-                path="/customers/new"
-                render={(props) => <NewCustomer {...props} />}
-              />
-              <Route
-                exact
-                path="/customers/:customerId"
-                render={(props) => <CustomerDetail {...props} />}
-              />
-              <Route
-                exact
-                path="/customers/:customerId/edit_profile"
-                render={(props) => <EditCustomer {...props} />}
-              />
-            </Routes>
-            <Message style={{ color: "#BBB" }} size="mini">
-              <Message.Header>
-                Environment Variable Debug Information
-              </Message.Header>
-              <p>
-                REACT_APP_CUSTOMER_SELF_SERVICE_BACKEND:{" "}
-                {customerSelfServiceBackend}
-                <br />
-                REACT_APP_CUSTOMER_MANAGEMENT_BACKEND:{" "}
-                {customerManagementBackend}
-                <br />
-                REACT_APP_POLICY_MANAGEMENT_FRONTEND: {policyManagementFrontend}
-              </p>
-            </Message>
-          </Container>
-        </BrowserRouter>
-      </Provider>
+function App(): React$Element {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Container text>
+                    <SiteHeader/>
+                    <MenuBar/>
+                    <Routes>
+                        <Route exact path="/" element={<Home/>}/>
+                        <Route exact path="/customers/new" element={<NewCustomer/>}/>
+                        <Route exact path="/customers/:customerId" element={<CustomerDetail/>}/>
+                        <Route exact path="/customers/:customerId/edit_profile" element={<EditCustomer/>}/>
+                    </Routes>
+                    <Message style={{color: "#BBB"}} size="mini">
+                        <Message.Header>
+                            Environment Variable Debug Information
+                        </Message.Header>
+                        <p>
+                            REACT_APP_CUSTOMER_SELF_SERVICE_BACKEND:{" "}
+                            {customerSelfServiceBackend}
+                            <br/>
+                            REACT_APP_CUSTOMER_MANAGEMENT_BACKEND:{" "}
+                            {customerManagementBackend}
+                            <br/>
+                            REACT_APP_POLICY_MANAGEMENT_FRONTEND: {policyManagementFrontend}
+                        </p>
+                    </Message>
+                </Container>
+            </BrowserRouter>
+        </Provider>
     )
-  }
 }
+
+export default App
