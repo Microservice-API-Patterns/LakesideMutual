22d7eed663dd080d166f0fb3a8ec4cf68a931017
Jan Ruch <j.ruch@hispeed.ch>
2024-12-04 22:59:38 +0100

    migrate spring-boot-admin to spring boot 3

--- spring-boot-admin/src/main/java/com/lakesidemutual/springbootadmin/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/com/lakesidemutual/springbootadmin/SpringBootAdminApplication.java
@@ -1,20 +1,17 @@
 package com.lakesidemutual.springbootadmin;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // The Spring Boot Admin application monitors the other sample applications and provides various insights. 
 // See the project's GitHub page: https://github.com/codecentric/spring-boot-admin
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 public class SpringBootAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
 }
